d9036f7bb1ca3d22079545a094ca0ccfb2ca1b48
beniv_bohdan <bohdanbeniv@gmail.com>
2022-06-20 20:52:45 +0200

    1 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import './App.css';
+import TodoList,{TaskType} from "./TodoList";
 
+
+//CRUD
 function App() {
+    const title_1:string = "Hello title 1 ";
+    const title_2:string = "Hello title 2";
+    const title_3:string = "Hello title 3";
+
+    const tasks_1: Array<TaskType> =[
+        {id: 1,title:"HTML", isDone:true},
+        {id: 2,title:"css", isDone:false},
+        {id: 3,title:"js", isDone:true},
+    ]
+    const tasks_2: Array<TaskType> =[
+        {id: 1,title:"SASS", isDone:true},
+        {id: 2,title:"ANGULAR", isDone:false},
+        {id: 3,title:"js", isDone:true},
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title={title_1} tasks={tasks_1}/>
+            <TodoList title={title_2} tasks={tasks_2}/>
+            <TodoList title={title_3} tasks={tasks_1}/>
         </div>
     );
 }
